02ed51baf2e3d75f2c600aecfbc75662592d50cb
Mohammad Reza Karimi <m.r.karimi.j@gmail.com>
2015-09-13 19:54:49 +0200

    edit concat script

--- data/concat.py
+++ data/concat.py
@@ -14,12 +14,17 @@ def datetime_to_timestamp(datetime_list):
 
 d = {}
 
-file_list = glob.glob(os.path.join(directory, 'tweet_times_*.pkl'))
-total = len(file_list)
+# file_list = glob.glob(os.path.join(directory, 'tweet_times_*.pkl'))
+# total = len(file_list)
+# progress = 0.
+
+start = 1000
+end = 2000
+
 progress = 0.
 
-for chunk in file_list:
-    f = open(chunk, 'rb')
+for i in range(start, end):
+    f = open('tweet_times_%d000021.pkl' % i, 'rb')
     loaded_dict = pickle.load(f)
     for k in loaded_dict:
         if k in d:
@@ -28,7 +33,7 @@ for chunk in file_list:
             d[k] = datetime_to_timestamp(loaded_dict[k])
 
     f.close()
-    progress += 100. / float(total)
+    progress += 100. / float(end-start)
     print('\r%.2f%%' % progress, end='')
 
 p_d = pnd.Series(d)

61c7a9235ff400102b5cd47685c78bbe3c95a9e8
Mohammad Reza Karimi <m.r.karimi.j@gmail.com>
2015-09-14 16:41:05 +0200

    models and simulate refactor

--- data/models.py
+++ data/models.py
@@ -1,50 +1,109 @@
+from __future__ import division
 import numpy as np
+from math import ceil
 
 
 class Intensity:
-    def __init__(self, size=0):
-        self.rates = np.zeros(size)
-        self.time_intervals = np.zeros(size)
+    """
+    The intensity unit is tweets per hour and time slots are also expressed in hour.
+    Intensity is of the form:
+        [{
+            "rate": intensity value in tweets per hour,
+            "length": time slot length in hours
+        },
+        ...]
+    """
 
-    def get_weekday_rates(self, weekday):
+    def __init__(self, rates=None):
         """
-        Assumes a weekly basis for rates and also equal values for time intervals
-        :param weekday: 0 for monday and so on
-        :return: the rates on that certain day
+        :param rates: if given a list, it means rates are given in tweets per hour and
+                      time slots are going to be 1 hour each
         """
+        if rates:
+            self.intensity = [{"rate": rate, "length": 1.} for rate in rates]
+        else:
+            self.intensity = []
 
-        intervals_per_day = 24 * 60 * 60 / self.time_intervals[0]
-        return self.rates[weekday * intervals_per_day:(weekday + 1) * intervals_per_day]
+    def append(self, rate, length):
+        self.intensity.append({"rate": rate, "length": length})
+
+    def size(self):
+        return len(self.intensity)
+
+    def total_time(self):
+        return sum([item['length'] for item in self.intensity])
+
+    def get_as_vector(self):
+        """
+        :return tuple of rates and lengths arrays in numpy format
+        """
+        return (np.array([item['rate'] for item in self.intensity]),
+                np.array([item['length'] for item in self.intensity]))
+
+    def __getitem__(self, item):
+        return self.intensity[item]
+
+    def __str__(self):
+        return ', '.join(['(%.3f, %.2f)' % (item['rate'], item['length']) for item in self.intensity])
 
 
 class TweetList:
-    def __init__(self):
-        self.tweet_times = []
+    def __init__(self, times=None):
+        """
+        Time is in unix timestamp format, and so is in seconds precision
+        :param times: time list, can be none
+        """
+        self.tweet_times = [] if times is None else times
 
     def __str__(self):
         return '%d tweets: ' % len(self.tweet_times) + str(self.tweet_times)
 
-    def get_weekly_intensity(self, interval_length_in_seconds=3600):
+    def __getitem__(self, item):
+        return self.tweet_times[item]
 
-        week = 60 * 60 * 24 * 7
+    def append_to(self, times):
+        self.tweet_times += times
 
-        total_time = (self.tweet_times[-1] - self.tweet_times[0]).total_seconds()
-        total_number_of_weeks = int(total_time / week)
+    def get_periodic_intensity(self, period_length=24 * 7, time_slots=None):
+        """
+        :param period_length: in hours, default is one week (must be an integer if time_slots is None)
+        :param time_slots: if not provided, default is equal time slots of one hour
+        :return: intensity in the period length
+        """
 
-        intensity = Intensity(size=week / interval_length_in_seconds)
+        if time_slots is None:
+            time_slots = [1.] * period_length
+
+        assert sum(time_slots) == period_length
+
+        total_time = (self[-1] - self[0]) / 3600.
+        total_number_of_periods = ceil(total_time / period_length)
+
+        tweets_per_slot = [0] * len(time_slots)
 
         for time in self.tweet_times:
-            intensity.time_intervals[find_interval(time, interval_length_in_seconds)] += 1.
+            tweets_per_slot[find_interval(time, period_length, time_slots)] += 1
 
-        for i in range(len(intensity.time_intervals)):
-            intensity.time_intervals[i] /= float(total_number_of_weeks)
+        intensity = Intensity()
+
+        for i in range(len(time_slots)):
+            intensity.append(rate=tweets_per_slot[i] / total_number_of_periods / time_slots[i],
+                             length=time_slots[i])
 
         return intensity
 
 
-def find_interval(tweet_time, dt):
-        weekday = tweet_time.weekday()
-        t = tweet_time.time()
-        seconds = t.second + t.minute * 60 + \
-                  t.hour * 60 * 60 + weekday * 60 * 60 * 24
-        return int(seconds / dt)
+def find_interval(tweet_time, period_length, time_slots):
+    time_in_period = tweet_time % (period_length * 3600)
+    t, i = 0, 0
+    while t < time_in_period:
+        t += time_slots[i] * 3600.
+        i += 1
+    return i - 1
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
--- simulator/simulate.py
+++ simulator/simulate.py
@@ -1,36 +1,47 @@
+from __future__ import division
 import numpy as np
+from data.models import Intensity
 
 
-def generate_poisson_process(rate, end_of_time_interval, beginning_of_time_interval):
+def generate_poisson_process(rate, time_start, time_end):
     """
-    This function will produce an array, which is a sample of a poisson process
+    This function will produce an array, which is a sample of a homogeneous poisson process
     with $\lambda = "rate"$ and begins at "beginning_of_time_interval" and ends at "end_of_time_interval"
     If rate is too low, just returns an empty list of events.
+
+    :param rate: expressed as tweet per hour
+    :param time_start: in hours
+    :param time_end: in hours
     """
     if rate < 0.1e-6:
         return []
 
-    last_event_time = beginning_of_time_interval
+    last_event_time = time_start
     points = []
 
     while True:
         time_to_next_event = np.random.exponential(1. / rate)
         last_event_time += time_to_next_event
-        if last_event_time < end_of_time_interval:
+        if last_event_time < time_end:
             points.append(last_event_time)
         else:
             break
     return points
 
 
-def create_piecewise_constant_poisson_process(intensity):
+def generate_piecewise_constant_poisson_process(intensity):
+    """
+    :type intensity: Intensity
+    :return: a list
+    """
     process = []
-    end_of_last_interval = 0
+    end_of_last_slot = 0
 
-    for i in range(len(intensity.rates)):
-        process += generate_poisson_process(intensity.rates[i], intensity.time_intervals[i] + end_of_last_interval,
-                                            end_of_last_interval)
-        end_of_last_interval += intensity.time_intervals[i]
+    for item in intensity.intensity:
+        process += generate_poisson_process(item['rate'],
+                                                      end_of_last_slot,
+                                                      item['length'] + end_of_last_slot)
+        end_of_last_slot += item['length']
 
     return process
 
@@ -88,14 +99,33 @@ def get_expectation_std_top_k_simulating(lambda1, lambda2, k, number_of_iteratio
     This function will simulate two poisson processes with rates $\lambda_1$ and $\lambda_2$ for "number_of_iterations"
     times and for each of the simulations computes the function "time_being_in_top_k"
     and after all computes the average and standard deviation of the results and returns them.
+
+    :type lambda1: Intensity
+    :type lambda2: Intensity
     """
 
     times_on_top = []
 
     for i in range(number_of_iterations):
-        process1 = create_piecewise_constant_poisson_process(lambda1)
-        process2 = create_piecewise_constant_poisson_process(lambda2)
+        process1 = generate_piecewise_constant_poisson_process(lambda1)
+        process2 = generate_piecewise_constant_poisson_process(lambda2)
 
-        times_on_top += [time_being_in_top_k(process1, process2, k, sum(lambda1.time_intervals))]
+        times_on_top += [time_being_in_top_k(process1, process2, k, lambda1.total_time())]
 
     return [np.mean(times_on_top), np.std(times_on_top)]
+
+
+def main():
+    from data.models import TweetList
+
+    lambda1 = Intensity([2, 14, 10, 5, 0.1, 0.2, 1])
+    tw_lst = TweetList()
+    for i in range(10):
+        proc = generate_piecewise_constant_poisson_process(lambda1)
+        proc = [t * 3600 + i * (3600 * 7) for t in proc]
+        tw_lst.append_to(proc)
+    print(tw_lst.get_periodic_intensity(7))
+
+
+if __name__ == '__main__':
+    main()

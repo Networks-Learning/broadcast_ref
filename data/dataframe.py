633bde090682ff1c7fe670cf9100f510e6db23bd
Mohammad Reza Karimi <moreka@dhcp-143-155.mpi-sws.org>
2015-09-15 15:03:55 +0200

    dataframe

--- data/dataframe.py
+++ data/dataframe.py
@@ -25,46 +25,44 @@ def datetime_to_timestamp(datetime_list):
     return [long(d.strftime('%s')) for d in datetime_list]
 
 
-file_id_start = 0
-file_id_end = 4382
-total = file_id_end - file_id_start + 1
-progress = 0.
-remaining = 0.
-t_start = 0.
-t_est = 0.
+def run(file_id_start=0, file_id_end=4382):
+    total = file_id_end - file_id_start + 1
+    progress = 0.
+    remaining = 0.
+    t_start = 0.
+    t_est = 0.
 
-for i in range(file_id_start, file_id_end + 1):
-    chunk = os.path.join(directory, 'tweet_times_%d000021.pkl' % i)
-    t_start = time.clock()
+    for i in range(file_id_start, file_id_end + 1):
+        chunk = os.path.join(directory, 'tweet_times_%d000021.pkl' % i)
+        t_start = time.clock()
+        try:
+            with open(chunk, 'rb') as f:
+                print('\r[%.2f%%] loading pickle %s ... (%s)' % (progress, chunk, datetime.timedelta(seconds=remaining)), end='')
+                sys.stdout.flush()
+                loaded_dict = pickle.load(f)
+                print('\r[%.2f%%] updating global dict... (%s)' % (progress, datetime.timedelta(seconds=remaining)) + ' ' * 60, end='')
+                sys.stdout.flush()
+                add_dict_to_db_dict(loaded_dict)
+
+        except (IOError, EOFError):
+            sys.stderr.write('IO/EOF Error loading pickle %s' % chunk)
+
+        progress += 100. / float(total)
+
+        if t_est is 0:   
+            t_est = (time.clock() - t_start)
+        else:
+            t_est = (t_est + time.clock() - t_start) / 2.0
+        
+        remaining = t_est * (100. - progress) * total / 100.
+
     try:
-        with open(chunk, 'rb') as f:
-            print('\r[%.2f%%] loading pickle %s ... (%s)' % (progress, chunk, datetime.timedelta(seconds=remaining)), end='')
-            sys.stdout.flush()
-            loaded_dict = pickle.load(f)
-            print('\r[%.2f%%] updating global dict... (%s)' % (progress, datetime.timedelta(seconds=remaining)) + ' ' * 50, end='')
-            sys.stdout.flush()
-            add_dict_to_db_dict(loaded_dict)
-
-    except (IOError, EOFError):
-        sys.stderr.write('IO/EOF Error loading pickle %s' % chunk)
-
-    progress += 100. / float(total)
-
-    if t_est is 0:   
-        t_est = (time.clock() - t_start)
-    else:
-        t_est = (t_est + time.clock() - t_start) / 2.0
-    
-    remaining = t_est * (100. - progress) * total / 100.
-
-try:
-    df = pd.DataFrame(db_dict)
-    store = pd.HDFStore('store.h5')
-    store['tweets'] = df
-    store.close()
-except:
-    sys.stderr.write('Error in saving to hdf! Pickling instead...')
-    with open('pickled_all.pkl', 'wb') as f:
-        pickle.dump(db_dict, f)
-
+        df = pd.DataFrame(db_dict)
+        store = pd.HDFStore('store.h5')
+        store['tweets'] = df
+        store.close()
+    except:
+        sys.stderr.write('Error in saving to hdf! Pickling instead...')
+        with open('pickled_all_%d.pkl' % file_id_start, 'wb') as f:
+            pickle.dump(db_dict, f)
 

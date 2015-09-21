8d7e4190ff018f51487ec86ebd81cd1eed39e2a8
Mohammad Reza Karimi <m.r.karimi.j@gmail.com>
2015-09-21 13:01:50 +0200

    sqlite connection finale

--- data/db_connector.py
+++ data/db_connector.py
@@ -1,22 +1,24 @@
-import psycopg2
+import sqlite3
+
 
 class DbConnection:
     def __init__(self):
-        self.con = psycopg2.connect(host='postgresql01.mpi-sws.org',
-                                    database='twitter',
-                                    user='twitter',
-                                    password='tweet@84')
+        self.con_db = sqlite3.connect('/local/moreka/db.sqlite3')
+        self.con_links = sqlite3.connect('/local/moreka/links.sqlite3')
 
     def __del__(self):
         self.close()
 
-    def get_cursor(self):
-        try:
-            cur = self.con.cursor()
-            return cur
-        except psycopg2.DatabaseError, e:
-            raise RuntimeError('Database Error %s' % e)
+    def get_cursor(self, tbl):
+        if tbl == 'tweets':
+            return self.con_db.cursor()
+        elif tbl == 'links':
+            return self.con_links.cursor()
+        else:
+            return None
 
     def close(self):
-        if self.con:
-            self.con.close()
+        if self.con_db:
+            self.con_db.close()
+        if self.con_links:
+            self.con_links.close()

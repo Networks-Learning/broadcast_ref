c06fb278d7d8d6aecb76f799d424cf87b269cb6c
Erfan <erfan.tavakoli.71@gmail.com>
2016-01-17 14:37:59 +0100

    works done in the brain :|

--- competitors/avm.py
+++ competitors/avm.py
@@ -1,11 +1,12 @@
 import random
 import numpy as np
-from competitors.utils import find_position, sampling
+from utils import *
 from data.db_connector import DbConnection
 from data.models import TweetList
 from operator import truediv
 from data.models import *
 from data.user import *
+import timeit
 
 __author__ = 'Rfun'
 
@@ -76,7 +77,7 @@ def gavm(self, budget, upper_bounds, time_slots=None, offset=0):
     return TweetList(generated_points)
 
 
-def baseline(self, user, budget, upper_bounds, method, time_slots=None, offset=0):
+def baseline(user, budget, upper_bounds, method, time_slots=None, offset=0):
     if time_slots is None:
         time_slots = [1] * (len(upper_bounds))
     generated_points = []
@@ -94,18 +95,26 @@ def baseline(self, user, budget, upper_bounds, method, time_slots=None, offset=0
         if method == 'iavm' or method == 'pavm':
             weights = np.array([0] * len(time_slots))
             for target in user.followers():
+                print('target %d' %(target.user_id()))
+                start = timeit.default_timer()
                 tweets = target.wall_tweet_list(user.user_id())
                 intensity = tweets.get_periodic_intensity().sub_intensity(offset, offset + total_time)
-
-                for i in range(len(intensity)):
+                stop = timeit.default_timer()
+                print('finished fetching for him at %.2f' %(stop-start))
+                
+                start = timeit.default_timer()
+                for i in range(len(intensity.get_as_vector()[0])):
                     if intensity[i]['rate'] < 0.0001:  # escaping from division by zero
                         intensity[i]['rate'] = 0.0001
                 if method == 'pavm':
-                    onlinity_probability = tweets.get_connection_probability()[offset, offset + total_time]
+                    onlinity_probability = tweets.get_connection_probability()[offset : offset + total_time]
                     weights += map(truediv, onlinity_probability, intensity.get_as_vector()[0])
                 else:
                     weights += map(truediv, [1]*len(intensity), intensity.get_as_vector()[0])
+                stop = timeit.default_timer()
+                print('finished creating for him at %.2f' %(stop-start))
 
+    print('started sampling')
 
     while len(generated_points) < real_budget:
         nominated_list = sampling(time_slots, weights, real_budget - len(generated_points))
@@ -114,7 +123,8 @@ def baseline(self, user, budget, upper_bounds, method, time_slots=None, offset=0
             if slots[position] + 1 <= upper_bounds[position]:
                 slots[position] += 1
                 generated_points += [time]
-
+    print('finished')
+    
     generated_points.sort()
     return generated_points
 

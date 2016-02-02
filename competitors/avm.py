cca83472d743dbd3fdea5215df762e8769b11e4a
Erfan Tavakoli <erfan.tavakoli71@gmail.com>
2016-02-02 12:44:51 +0330

    "ravm competitor is added"

--- competitors/avm.py
+++ competitors/avm.py
@@ -1,12 +1,10 @@
+from __future__ import division
 from competitors.utils import find_position, sampling
 from operator import truediv
-from data.models import *
-from data.user import *
 import numpy as np
 import timeit
 
 __author__ = 'Rfun'
-
 
 METHOD_RAVM = 0
 METHOD_IAVM = 1
@@ -41,11 +39,11 @@ def baseline(user, budget, upper_bounds, method, time_slots=None, offset=0):
         if method == 'iavm' or method == 'pavm':
             weights = np.array([0.] * len(time_slots))
             for target in user.followers():
-                print 'started fetching for %d with %d followee' %(target.user_id(), len(target.followees()))
+                print 'started fetching for %d with %d followee' % (target.user_id(), len(target.followees()))
                 start = timeit.default_timer()
                 tweets = target.wall_tweet_list(user.user_id())
                 stop = timeit.default_timer()
-                print '%.2f time for fetching' %(stop - start)
+                print '%.2f time for fetching' % (stop - start)
                 print 'tweets: ', tweets
                 p_intensity = tweets.get_periodic_intensity()
                 intensity = p_intensity.sub_intensity(offset, offset + total_time)
@@ -54,8 +52,8 @@ def baseline(user, budget, upper_bounds, method, time_slots=None, offset=0):
                     if intensity[i]['rate'] < 0.0001:  # escaping from division by zero
                         intensity[i]['rate'] = 0.0001
                 if method == 'pavm':
-                        onlinity_probability = target.tweet_list().get_connection_probability()[offset : offset + total_time]
-                        weights += map(truediv, onlinity_probability, intensity.get_as_vector()[0])
+                    onlinity_probability = target.tweet_list().get_connection_probability()[offset: offset + total_time]
+                    weights += map(truediv, onlinity_probability, intensity.get_as_vector()[0])
                 else:
                     weights += map(truediv, [1] * len(intensity), intensity.get_as_vector()[0])
     while len(generated_points) < real_budget:
@@ -70,8 +68,29 @@ def baseline(user, budget, upper_bounds, method, time_slots=None, offset=0):
     return generated_points
 
 
+def ravm(budget, upper_bounds):
+    if not isinstance(upper_bounds, np.array):
+        upper_bounds = np.array(upper_bounds)
+    budget = min(budget, sum(upper_bounds))
+    remained_budget = budget
+
+    result = np.zeros(len(upper_bounds))
+
+    while remained_budget > 1e-6:
+
+        share = remained_budget / (len(upper_bounds))
+        for i in range(len(upper_bounds)):
+            if upper_bounds[i] - result[i] > 1e-6:
+                temp = min(share, upper_bounds[i] - result[i])
+                result[i] += temp
+                remained_budget -= temp
+
+    return result
+
+
 def main():
     pass
+
 
 if __name__ == '__main__':
     main()

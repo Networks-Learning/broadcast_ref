7b2b24da380f1a6beb6f3a3dd9725d14043a6da9
Utkarsh Upadhyay <musically.ut@gmail.com>
2016-04-27 23:53:18 +0200

    Move code to a broadcast package.

--- setup.py
+++ setup.py
@@ -4,9 +4,16 @@ import numpy
 from Cython.Build import cythonize
 from setuptools import Extension
 
-extensions = [Extension("opt", ["opt/*.pyx"]), Extension("data", ["data/*.pyx"])]
+extensions = [Extension("opt", ["broadcast/opt/*.pyx"]),
+              Extension("data", ["broadcast/data/*.pyx"])]
 
 setup(
+    name='broadcast_ref',
+    version='0.0.1',
+    packages=['broadcast'],
+    package_dir={
+        'broadcast': 'broadcast'
+    },
     ext_modules=cythonize(extensions),
     include_dirs=[numpy.get_include()]
 )
